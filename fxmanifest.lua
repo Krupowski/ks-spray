efacb0a1fd383c481f339ad032c0072aea0ea50c
Krupowski <fqbixirys@gmail.com>
2024-10-15 13:58:53 +0200

    fxmanifest.lua

---
+++ fxmanifest.lua
@@ -0,0 +1,22 @@
+fx_version 'bodacious'
+game 'gta5'
+
+author 'Krupowski Studio 
+description 'Boski skrypt na analne zabawy'
+version '1.0.0'
+
+client_scripts {
+    'client/main.lua'
+}
+
+server_scripts {
+    '@mysql-async/lib/MySQL.lua',
+    'server/main.lua'
+}
+
+dependencies {
+    'es_extended',
+    'ox_lib',
+    'ox_inventory',
+    'ox_target'
+}
