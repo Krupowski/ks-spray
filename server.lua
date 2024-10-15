9d8b2b7d2e2dfde89348418461abadfd25ba2b5a
Krupowski <fqbixirys@gmail.com>
2024-10-15 14:09:54 +0200

    server.lua

--- server.lua
+++ server.lua
@@ -3,7 +3,7 @@ ESX = exports[“es_extended”]:getSharedObject()
 
 ESX.RegisterServerCallback('ks-spray:hasSprayCan', function(source, cb)
     local xPlayer = ESX.GetPlayerFromId(source)
-    local sprayCan = xPlayer.getInventoryItem('spray_can')
+    local sprayCan = xPlayer.getInventoryItem('spray')
 
     if sprayCan and sprayCan.count > 0 then
         cb(true)
@@ -15,7 +15,7 @@ end)
 
 RegisterNetEvent('ks-spray:removeSprayCan', function()
     local xPlayer = ESX.GetPlayerFromId(source)
-    xPlayer.removeInventoryItem('spray_can', 1)
+    xPlayer.removeInventoryItem('spray', 1)
 end)
 
 
