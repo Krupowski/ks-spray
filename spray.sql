a7c10b258c6706a907a83609f68560c53bdc7022
Krupowski <fqbixirys@gmail.com>
2024-10-15 18:34:51 +0200

    spray.sql

--- spray.sql
+++ spray.sql
@@ -1,1 +1,5 @@
-INSERT INTO items (name, label, weight, rare, can_remove) VALUES ('spray', 'Spray', 500, 0, 1);
+INSERT INTO items (name, label, weight, rare, can_remove) 
+VALUES ('spray', 'Spray Can', 500, 0, 1);
+
+INSERT INTO items (name, label, weight, rare, can_remove) 
+VALUES ('sponge', 'Sponge', 200, 0, 1);
